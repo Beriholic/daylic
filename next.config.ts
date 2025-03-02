4e4bfac8d31288e8086d7a95a6998f0504a3d0db
Beriholic <beriholic@petalmail.com>
2025-03-02 15:06:58 +0800

    refactor: :recycle:(next config) remove api rewrites configuration

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const rewrites = async () => {
-  return [
-    {
-      source: "/api/:path*",
-      destination: "http://localhost:8080/api/:path*",
-    },
-  ];
-};
-
 const nextConfig: NextConfig = {
   output: "standalone",
-  rewrites,
 };
 
 export default nextConfig;
